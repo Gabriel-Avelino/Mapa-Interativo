df6d994446e43b2da76db860e155c6352272e9a9
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-10-16 17:15:11 -0300

    Pesquisa de endereços

--- map/views.py
+++ map/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
 import folium
+from opencage.geocoder import OpenCageGeocode
 
 # Create your views here.
+
+# Insira aqui sua chave de API do OpenCage
+opencage_key = 'a5e06dcd00a54172bf18379c16c15c2a'
+
 def index(request):
-    # Create Map Object
-    m = folium.Map(location=[19, -12], zoom_start=2)
-    # Get HTML representation of Map Object
+    address = request.POST.get('address')
+    geocoder = OpenCageGeocode(opencage_key)
+
+    # Faz a requisição ao OpenCage para o endereço
+    if address == '' or address == None:
+        firstAddress = 'Brazil'
+        address = firstAddress
+    else:
+        firstAddress = ''
+    result = geocoder.geocode(address)
+
+    if result and len(result) and firstAddress != 'Brazil':
+        lat = result[0]['geometry']['lat']
+        lng = result[0]['geometry']['lng']
+        country = result[0]['components'].get('country', 'Unknown')
+        m = folium.Map(location=[lat, lng], zoom_start=16.0)
+        folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
+    else:
+        # Se a localização não for encontrada, usa valores padrão (Brasil)
+        lat, lng = -14.2350, -51.9253
+        country = 'Brazil'
+        m = folium.Map(location=[lat, lng], zoom_start=4.0)
+
+    # Gerando a representação HTML do mapa para o template
     m = m._repr_html_()
+
     context = {
         'm': m,
     }
+
     return render(request, 'index.html', context)
