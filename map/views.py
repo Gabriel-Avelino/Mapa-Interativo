0f1e53e2253850391da482deded141a9e4e1ab02
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-10-17 17:07:38 -0300

    Coletar dados de marcações

--- map/views.py
+++ map/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 import folium
 from opencage.geocoder import OpenCageGeocode
-
-# Create your views here.
+from django.http import JsonResponse
 
 # Insira aqui sua chave de API do OpenCage
 opencage_key = 'a5e06dcd00a54172bf18379c16c15c2a'
-
+geocoder = OpenCageGeocode(opencage_key)
 def index(request):
     address = request.POST.get('address')
-    geocoder = OpenCageGeocode(opencage_key)
+
 
     # Faz a requisição ao OpenCage para o endereço
     if address == '' or address == None:
@@ -22,13 +21,30 @@ def index(request):
     if result and len(result) and firstAddress != 'Brazil':
         lat = result[0]['geometry']['lat']
         lng = result[0]['geometry']['lng']
-        country = result[0]['components'].get('country', 'Unknown')
-        m = folium.Map(location=[lat, lng], zoom_start=16.0)
-        folium.Marker([lat, lng], tooltip='Click for more', popup=country).add_to(m)
+
+        if 'road' in result[0]['components']:
+            location = result[0]['components']['road']
+            zoom = 16  # Zoom mais próximo para estrada
+        elif 'municipality' in result[0]['components']:
+            location = result[0]['components']['municipality']
+            zoom = 12  # Zoom intermediário para cidade
+        elif 'state' in result[0]['components']:
+            location = result[0]['components']['state']
+            zoom = 6  # Zoom mais afastado para estado
+        elif 'country' in result[0]['components']:
+            location = result[0]['components']['country']
+            zoom = 4  # Zoom mais afastado para país
+        else:
+            location = 'Localização Desconhecida'
+            zoom = 2  # Zoom muito afastado, pois o local não foi identificado corretamente
+
+        # Cria o mapa com o zoom apropriado
+        m = folium.Map(location=[lat, lng], zoom_start=zoom)
+        folium.Marker([lat, lng], tooltip='Click for more', popup=location, draggable=True).add_to(m) 
+         
     else:
         # Se a localização não for encontrada, usa valores padrão (Brasil)
         lat, lng = -14.2350, -51.9253
-        country = 'Brazil'
         m = folium.Map(location=[lat, lng], zoom_start=4.0)
 
     # Gerando a representação HTML do mapa para o template
