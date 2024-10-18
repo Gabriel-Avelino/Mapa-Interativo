d64eab786dc052b5453a97f1965338f33b8c9bbb
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-10-18 13:34:44 -0300

    Atualização de mapa completa

--- map/views.py
+++ map/views.py
@@ -1,57 +1,50 @@
 from django.shortcuts import render
-import folium
 from opencage.geocoder import OpenCageGeocode
 from django.http import JsonResponse
 
-# Insira aqui sua chave de API do OpenCage
+# Chave API do OpenCage
 opencage_key = 'a5e06dcd00a54172bf18379c16c15c2a'
 geocoder = OpenCageGeocode(opencage_key)
+
 def index(request):
-    address = request.POST.get('address')
-
-
-    # Faz a requisição ao OpenCage para o endereço
-    if address == '' or address == None:
-        firstAddress = 'Brazil'
-        address = firstAddress
-    else:
-        firstAddress = ''
+    address = request.POST.get('address')  # Usa 'Brazil' como valor padrão
+    if address == None:
+        address = "Brazil"
     result = geocoder.geocode(address)
 
-    if result and len(result) and firstAddress != 'Brazil':
+    if result:
         lat = result[0]['geometry']['lat']
         lng = result[0]['geometry']['lng']
-
         if 'road' in result[0]['components']:
-            location = result[0]['components']['road']
             zoom = 16  # Zoom mais próximo para estrada
         elif 'municipality' in result[0]['components']:
-            location = result[0]['components']['municipality']
             zoom = 12  # Zoom intermediário para cidade
         elif 'state' in result[0]['components']:
-            location = result[0]['components']['state']
             zoom = 6  # Zoom mais afastado para estado
         elif 'country' in result[0]['components']:
-            location = result[0]['components']['country']
             zoom = 4  # Zoom mais afastado para país
         else:
-            location = 'Localização Desconhecida'
-            zoom = 2  # Zoom muito afastado, pois o local não foi identificado corretamente
-
-        # Cria o mapa com o zoom apropriado
-        m = folium.Map(location=[lat, lng], zoom_start=zoom)
-        folium.Marker([lat, lng], tooltip='Click for more', popup=location, draggable=True).add_to(m) 
-         
+            zoom = 2
+        location = result[0]['formatted']
     else:
-        # Se a localização não for encontrada, usa valores padrão (Brasil)
-        lat, lng = -14.2350, -51.9253
-        m = folium.Map(location=[lat, lng], zoom_start=4.0)
+        lat, lng, zoom = -14.2350, -51.9253, 4  # Coordenadas padrão do Brasil
+        location = "Brazil"
 
-    # Gerando a representação HTML do mapa para o template
-    m = m._repr_html_()
+    # Substituição do request.is_ajax()
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return JsonResponse({
+            'lat': lat,
+            'lng': lng,
+            'zoom': zoom,
+            'location': location
+        })
 
+    # Renderiza a página HTML com os dados iniciais
     context = {
-        'm': m,
+        'lat': lat,
+        'lng': lng,
+        'zoom': zoom,
+        'location': location
     }
 
     return render(request, 'index.html', context)
