c2994f1762184fb683a25436423b34fc972e3e1a
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-10-18 17:11:30 -0300

    Fim do sistema de pesquisa

--- map/views.py
+++ map/views.py
@@ -9,26 +9,31 @@ geocoder = OpenCageGeocode(opencage_key)
 def index(request):
     address = request.POST.get('address')  # Usa 'Brazil' como valor padrão
     if address == None:
-        address = "Brazil"
+        address = "Brasil"
     result = geocoder.geocode(address)
 
     if result:
         lat = result[0]['geometry']['lat']
         lng = result[0]['geometry']['lng']
         if 'road' in result[0]['components']:
+            location = result[0]['components']['road']
             zoom = 16  # Zoom mais próximo para estrada
         elif 'municipality' in result[0]['components']:
+            location = result[0]['components']['municipality']
             zoom = 12  # Zoom intermediário para cidade
         elif 'state' in result[0]['components']:
+            location = result[0]['components']['state']
             zoom = 6  # Zoom mais afastado para estado
         elif 'country' in result[0]['components']:
+            location = result[0]['components']['country']
             zoom = 4  # Zoom mais afastado para país
         else:
-            zoom = 2
-        location = result[0]['formatted']
+            location = result[0]['formatted']
+            zoom = 4
+
     else:
         lat, lng, zoom = -14.2350, -51.9253, 4  # Coordenadas padrão do Brasil
-        location = "Brazil"
+        location = "Brasil"
 
     # Substituição do request.is_ajax()
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
