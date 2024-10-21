db5b8082131068949031f61eef9a3b714c03bd93
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2024-10-21 08:24:36 -0300

    Início do sistema de mostrar horários

--- map/models.py
+++ map/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Search(models.Model):
+class Horario(models.Model):
     address = models.CharField(max_length=256, null=True)
-    data = models.DateTimeField(auto_now_add=True)
+    segunda_diurno = models.TimeField(null=True, blank=True)
+    segunda_noturno = models.TimeField(null=True, blank=True)
+    terca_diurno = models.TimeField(null=True, blank=True)
+    terca_noturno = models.TimeField(null=True, blank=True)
+    quarta_diurno = models.TimeField(null=True, blank=True)
+    quarta_noturno = models.TimeField(null=True, blank=True)
+    quinta_diurno = models.TimeField(null=True, blank=True)
+    quinta_noturno = models.TimeField(null=True, blank=True)
+    sexta_diurno = models.TimeField(null=True, blank=True)
+    sexta_noturno = models.TimeField(null=True, blank=True)
+    sabado_diurno = models.TimeField(null=True, blank=True)
+    sabado_noturno = models.TimeField(null=True, blank=True)
+    domingo_diurno = models.TimeField(null=True, blank=True)
+    domingo_noturno = models.TimeField(null=True, blank=True)
 
     def __str__(self):
         return self.address
